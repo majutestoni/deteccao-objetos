d3e33e69834577365c46244c8174c9cba12b0322
Martin Lange de Assis <101527940+Martin-Lange-Assis@users.noreply.github.com>
2025-05-11 12:05:04 -0300

    Update main2.py
    - Melhoria na detecção de objetos

--- main2.py
+++ main2.py
@@ -1,18 +1,22 @@
 import cv2  # Biblioteca OpenCV para processar imagens e vídeos
 from ultralytics import YOLO  # Importa a classe do modelo YOLO da biblioteca Ultralytics
+from google.colab.patches import cv2_imshow
 import time  # Biblioteca para pausar a execução (usada para dar tempo de ver os frames)
 
 # Carrega o modelo YOLOv8 versão "nano" (leve e rápido), já pré-treinado
-model = YOLO('yolov8m.pt')
+# model = YOLO('yolov8m.pt')
+model = YOLO('yolov8x.pt')  # maior e mais preciso
 
-# Definição das classes que iremos focar 
+
+
+# Definição das classes que iremos focar
 target_classes = {'car', 'truck'}
 car_count = 0
 truck_count = 0
 
-# Linha para realizarmos a contagem 
+# Linha para realizarmos a contagem
 line_y = 300  # Posição vertical da linha
-offset = 10   # Margem de tolerância para a linha
+offset = 40   # Margem de tolerância para a linha
 
 # Abre o arquivo de vídeo para leitura
 cap = cv2.VideoCapture("fast motion cars moving on highway.mp4")
@@ -34,26 +38,38 @@ while cap.isOpened():
     # Só processa e mostra a cada 5 frames
     if frame_count % 10 == 0:
         # Aplica a detecção de objetos no frame atual
-        results = model.predict(source=frame, imgsz=736, conf=0.63, verbose=False)
+        results = model.predict(source=frame, imgsz=736, conf=0.55, verbose=False)
+        # results = model.predict(source=frame, imgsz=736, conf=0.5, verbose=False)
         detections = results[0].boxes
         annotated_frame = frame.copy()
-        
+
         cv2.line(annotated_frame, (0, line_y), (annotated_frame.shape[1], line_y), (0, 255, 255), 2)
         if detections is not None:
             for i, box in enumerate(detections.xyxy.cpu().numpy()):
                 x1, y1, x2, y2 = map(int, box[:4])
                 class_id = int(detections.cls[i].cpu().numpy())
                 label = class_names[class_id]
+                print(label)  # <--- Veja se 'truck' aparece
 
                 if label not in target_classes:
                     continue  # Ignora outras classes
-                
+
                 # Centro do objeto
-                cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+                # cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+                cx = (x1 + x2) // 2
                 # Desenha o retângulo e centro
                 cv2.rectangle(annotated_frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                # cv2.circle(annotated_frame, (cx, cy), 3, (0, 0, 255), -1)
+                # cx = (x1 + x2) // 2
+
+                if label == 'car':
+                  cy = (y1 + y2) // 2  # centro vertical da caixa
+                elif label == 'truck':
+                  cy = y2  # base da caixa (parte de baixo do caminhão)
+
                 cv2.circle(annotated_frame, (cx, cy), 3, (0, 0, 255), -1)
-                
+
+
                 # Identificador para evitar dupla contagem
                 track_id = f"{cx}-{cy}-{class_id}"
                 if (line_y - offset) < cy < (line_y + offset) and track_id not in counted_ids:
@@ -63,17 +79,14 @@ while cap.isOpened():
                     elif label == 'truck':
                         truck_count += 1
 
-                            
+
         cv2.putText(annotated_frame, f"Carros: {car_count}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.putText(annotated_frame, f"Caminhoes: {truck_count}", (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 165, 255), 2)
 
-        cv2.imshow("Contador de Veículos", annotated_frame)
-        if cv2.waitKey(1) == 27:  # ESC
-            break
-        
+        cv2_imshow(annotated_frame)
+
         # Dá uma pausa de 0.2 segundos para que a imagem possa ser vista
         time.sleep(0.5)
 
 # Libera o vídeo e fecha as janelas (mesmo que não sejam usadas no Colab)
 cap.release()
-cv2.destroyAllWindows()
